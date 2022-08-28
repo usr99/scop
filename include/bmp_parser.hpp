4050b20a720e1468c23c9ee516c667a40eb31dea
mamartin <mamartin@student.42.fr>
2022-08-28 15:58:15 -0400

    bmp parser

---
+++ include/bmp_parser.hpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bmp_parser.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamartin <mamartin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 14:59:56 by mamartin          #+#    #+#             */
+/*   Updated: 2022/08/28 15:28:38 by mamartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _BMP_PARSER_HPP_
+# define _BMP_PARSER_HPP_
+
+# include <vector>
+# include <cstdint>
+
+struct BMPimage
+{
+	BMPimage(const std::string& filename);
+
+# pragma pack(push, 1)
+
+	struct FileHeader
+	{
+		uint16_t type;
+		uint32_t size;
+		uint32_t reserved;
+		uint32_t offset;
+	};
+
+	struct InfoHeader
+	{
+		uint32_t size;
+		int32_t  width;
+		int32_t  height;
+		uint16_t planes;
+		uint16_t bpp;
+		uint32_t compression;
+	};
+
+# pragma pack(pop)
+
+	const uint8_t* data() const;
+
+	FileHeader file;
+	InfoHeader info;
+
+	private:
+
+		bool					_M_IsReversed;
+		std::vector<uint8_t>	_M_Pixels;
+};
+
+#endif
---
+++ src/bmp_parser.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bmp_parser.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamartin <mamartin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/28 15:06:04 by mamartin          #+#    #+#             */
+/*   Updated: 2022/08/28 15:57:25 by mamartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdexcept>
+#include <fstream>
+#include <iostream>
+#include "bmp_parser.hpp"
+
+BMPimage::BMPimage(const std::string& filename)
+{
+	std::ifstream ifs(filename, std::ios::binary);
+	if (ifs.fail())
+		throw std::runtime_error(filename + ": failed to open image");
+
+	ifs.read(reinterpret_cast<char*>(&file), sizeof(FileHeader));
+	if (file.type != 0x4D42) // should be "BM"
+		throw std::runtime_error(filename + ": incorrect file type");
+
+	ifs.read(reinterpret_cast<char*>(&info), sizeof(InfoHeader));
+
+	std::cout << info.size << '\n';
+
+	if (info.size != 40 && info.size != 108 && info.size != 124)
+		throw std::runtime_error(filename + ": incorrect DIB format");
+	if (info.bpp != 24 && info.bpp != 32)
+		throw std::runtime_error(filename + ": only 24 and 32 bits images are supported");
+	if (info.compression != 0 && info.compression != 3)
+		throw std::runtime_error(filename + ": data compression is not supported");
+	_M_IsReversed = (info.height < 0);
+	info.height = std::abs(info.height);
+
+	ifs.seekg(file.offset, ifs.beg);
+	_M_Pixels.resize(info.width * info.height * 4, 0xFF);
+
+	const uint8_t bytesPerPixel = info.bpp / 8;
+	const size_t stride = info.width * bytesPerPixel;
+	const uint8_t padding = stride % 4;
+
+	std::cout << _M_IsReversed << '\n';
+
+	for (int y = 0; y < info.height; y++)
+	{
+		for (int x = 0; x < info.width; x++)
+		{
+			size_t index;
+			if (_M_IsReversed)
+				index = (y * info.width + x) * 4;
+			else
+				index = ((info.height - (y + 1)) * info.width + x) * 4;
+			ifs.read(reinterpret_cast<char*>(_M_Pixels.data() + index), bytesPerPixel);
+		}
+		ifs.seekg(padding, ifs.cur);
+	}
+
+	ifs.close();
+}
+
+const uint8_t*
+BMPimage::data() const
+{
+	return _M_Pixels.data();
+}
